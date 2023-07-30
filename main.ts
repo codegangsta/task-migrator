20bbca43771aab30629140bfc6dd1ca12bf829fd
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-07-30 11:41:49 -0700

    Made some timezone fixes

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
-import * as chrono from 'chrono-node';
-import { App, Editor, getIcon, MarkdownView, Notice, Plugin, PluginSettingTab, Setting, SuggestModal, TFile } from 'obsidian';
+import * as chrono from "chrono-node";
+import {
+	App,
+	Editor,
+	getIcon,
+	MarkdownView,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	SuggestModal,
+	TFile,
+} from "obsidian";
 
 interface TaskMigratorSettings {
 	mySetting: string;
@@ -8,12 +18,12 @@ interface TaskMigratorSettings {
 interface MigrationTarget {
 	filePath: string;
 	date?: string;
-	file?: TFile
+	file?: TFile;
 }
 
 const DEFAULT_SETTINGS: TaskMigratorSettings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+};
 
 export default class TaskMigrator extends Plugin {
 	settings: TaskMigratorSettings;
@@ -22,32 +32,34 @@ export default class TaskMigrator extends Plugin {
 		await this.loadSettings();
 
 		this.addCommand({
-			id: 'migrate-task',
-			name: 'Migrate a task to another file',
-			icon: 'calendar-plus',
+			id: "migrate-task",
+			name: "Migrate a task to another file",
+			icon: "calendar-plus",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				new OptionsModal(this.app, ">").open()
-			}
-		})
+				new OptionsModal(this.app, ">").open();
+			},
+		});
 
 		this.addCommand({
-			id: 'scheduel-task',
-			name: 'Schedule a task to another file',
-			icon: 'calendar-plus',
+			id: "schedule-task",
+			name: "Schedule a task to another file",
+			icon: "calendar-plus",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				new OptionsModal(this.app, "<").open()
-			}
-		})
+				new OptionsModal(this.app, "<").open();
+			},
+		});
 
 		this.addSettingTab(new TaskMigratorSettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -56,171 +68,206 @@ export default class TaskMigrator extends Plugin {
 }
 
 class OptionsModal extends SuggestModal<MigrationTarget> {
-	type: '>' | '<'
+	type: ">" | "<";
 
-	constructor(app: App, type: '>' | '<') {
-		super(app)
-		this.type = type
-		this.emptyStateText = "No files found"
+	constructor(app: App, type: ">" | "<") {
+		super(app);
+		this.type = type;
+		this.emptyStateText = "No files found";
 
-		if (type === '>') {
-			this.setPlaceholder("Migrate this task to...")
-		} else if (type === '<') {
-			this.setPlaceholder("Schedule this task to...")
+		if (type === ">") {
+			this.setPlaceholder("Migrate this task to...");
+		} else if (type === "<") {
+			this.setPlaceholder("Schedule this task to...");
 		}
 	}
 
 	// parses a suggestion using chrono, if there is a hit, add it as a first suggestion
 	// otherwise, return all files in the vault, filtered by the query
-	getSuggestions(query: string): MigrationTarget[] | Promise<MigrationTarget[]> {
-		const { format, folder, template } = window.app.plugins.getPlugin("periodic-notes")?.settings?.daily || {};
+	getSuggestions(
+		query: string
+	): MigrationTarget[] | Promise<MigrationTarget[]> {
+		const { folder } =
+			// @ts-ignore
+			window.app.plugins.getPlugin("periodic-notes")?.settings?.daily ||
+			{};
 		return new Promise((resolve) => {
-			query = query.toLowerCase()
-			let dateQuery = query
+			query = query.toLowerCase();
+			let dateQuery = query;
 			if (query.startsWith("tod")) {
-				dateQuery = "Today"
+				dateQuery = "Today";
 			}
 			if (query.startsWith("tom")) {
-				dateQuery = "Tomorrow"
+				dateQuery = "Tomorrow";
 			}
 			if (query.startsWith("yes")) {
-				dateQuery = "Yesterday"
+				dateQuery = "Yesterday";
 			}
 			if (query.startsWith("mon")) {
-				dateQuery = "Monday"
+				dateQuery = "Monday";
 			}
 			if (query.startsWith("tue")) {
-				dateQuery = "Tuesday"
+				dateQuery = "Tuesday";
 			}
 			if (query.startsWith("wed")) {
-				dateQuery = "Wednesday"
+				dateQuery = "Wednesday";
 			}
 			if (query.startsWith("thu")) {
-				dateQuery = "Thursday"
+				dateQuery = "Thursday";
 			}
 			if (query.startsWith("fri")) {
-				dateQuery = "Friday"
+				dateQuery = "Friday";
 			}
 			if (query.startsWith("sat")) {
-				dateQuery = "Saturday"
+				dateQuery = "Saturday";
 			}
 			if (query.startsWith("sun")) {
-				dateQuery = "Sunday"
+				dateQuery = "Sunday";
 			}
 
-			query = query.toLowerCase()
-			const parsed = chrono.parseDate(dateQuery)
-			const date = parsed ? parsed.toISOString().split('T')[0] : null
+			query = query.toLowerCase();
+			const parsed = chrono.parseDate(dateQuery, new Date(), {
+				// @ts-ignore
+				timezone: this.app.vault.getConfig("locale").timezone,
+			});
+			const date = parsed ? parsed.toISOString().split("T")[0] : null;
 
-			const files: MigrationTarget[] = this.app.vault.getMarkdownFiles()
+			const files: MigrationTarget[] = this.app.vault
+				.getMarkdownFiles()
 				.filter((file) => file.path.toLowerCase().includes(query))
-				.map((file) => ({ filePath: file.path, file: file }))
+				.map((file) => ({ filePath: file.path, file: file }));
 
 			// check if a file with the date already exists in "Daily Logs" folder
 			if (parsed) {
-				const path = folder + "/" + date + ".md"
-				const file = this.app.vault.getMarkdownFiles().find((file) => file.path === path)
-				files.unshift({ filePath: path, date: dateQuery, file: file })
+				const path = folder + "/" + date + ".md";
+				const file = this.app.vault
+					.getMarkdownFiles()
+					.find((file) => file.path === path);
+				files.unshift({ filePath: path, date: dateQuery, file: file });
 			}
 
-			resolve(files)
-		})
+			resolve(files);
+		});
 	}
 	renderSuggestion(value: MigrationTarget, el: HTMLElement) {
-		el.addClass('mod-complex')
+		el.addClass("mod-complex");
 
-		const content = el.createDiv('suggestion-content')
-		content.appendText(value.filePath)
-		el.appendChild(content)
+		const content = el.createDiv("suggestion-content");
+		content.appendText(value.filePath);
+		el.appendChild(content);
 
-		const aux = el.createDiv('suggestion-aux')
-		el.appendChild(aux)
+		const aux = el.createDiv("suggestion-aux");
+		el.appendChild(aux);
 
 		if (value.date) {
-			const flair = el.createDiv('suggestion-flair')
-			flair.setText(value.date)
-			aux.appendChild(flair)
+			const flair = el.createDiv("suggestion-flair");
+			flair.setText(value.date);
+			aux.appendChild(flair);
 		}
 		if (!value.file) {
-			const icon = getIcon("calendar-plus")
+			const icon = getIcon("calendar-plus");
 			if (icon) {
-				const flair = el.createDiv('suggestion-flair')
-				flair.appendChild(icon)
-				aux.appendChild(flair)
+				const flair = el.createDiv("suggestion-flair");
+				flair.appendChild(icon);
+				aux.appendChild(flair);
 			}
 		}
-
-
 	}
 
-	async onChooseSuggestion(item: MigrationTarget, evt: MouseEvent | KeyboardEvent) {
+	async onChooseSuggestion(
+		item: MigrationTarget,
+		evt: MouseEvent | KeyboardEvent
+	) {
 		if (!item.file) {
-			item.file = await this.app.vault.create(item.filePath, "")
+			item.file = await this.app.vault.create(item.filePath, "");
 		}
 
-
-		const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor
+		const editor =
+			this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
 		if (!editor) {
-			console.log("No active editor")
-			return
+			console.log("No active editor");
+			return;
 		}
 
-
-		const lines: number[] = []
+		const lines: number[] = [];
 		// loop through all lines in the current selection, if there are no lines then use the current line
-		const selections = editor.listSelections()
+		const selections = editor.listSelections();
 		if (selections.length === 0) {
-			lines.push(editor.getCursor().line)
+			lines.push(editor.getCursor().line);
 		} else {
 			selections.forEach((selection) => {
 				if (selection.head.line < selection.anchor.line) {
-					for (let i = selection.head.line; i <= selection.anchor.line; i++) {
-						lines.push(i)
+					for (
+						let i = selection.head.line;
+						i <= selection.anchor.line;
+						i++
+					) {
+						lines.push(i);
 					}
 				} else {
-					for (let i = selection.anchor.line; i <= selection.head.line; i++) {
-						lines.push(i)
+					for (
+						let i = selection.anchor.line;
+						i <= selection.head.line;
+						i++
+					) {
+						lines.push(i);
 					}
 				}
-			})
+			});
 		}
 
-		const taskContent: string[] = []
+		const taskContent: string[] = [];
 
 		lines.forEach(async (lineNumber) => {
-			const line = editor.getLine(lineNumber)
-			const taskRegex = /(- \[(.)\] )(.*)/
-			const taskMatch = line.match(taskRegex)
+			const line = editor.getLine(lineNumber);
+			const taskRegex = /(- \[(.)\] )(.*)/;
+			const taskMatch = line.match(taskRegex);
 			if (!taskMatch) {
-				console.log("No task found")
-				return
+				console.log("No task found");
+				return;
 			}
 
 			// create a markdown link
-			const link = this.app.fileManager.generateMarkdownLink(item.file, item.filePath)
-			const newLine = line.replace(`- [${taskMatch[2]}] `, `- [${this.type}] `) + " " + link
-			editor.setLine(lineNumber, newLine)
-			taskContent.push(line)
-
-		})
+			const link = this.app.fileManager.generateMarkdownLink(
+				item.file,
+				item.filePath
+			);
+			const newLine =
+				line.replace(`- [${taskMatch[2]}] `, `- [${this.type}] `) +
+				" " +
+				link;
+			editor.setLine(lineNumber, newLine);
+			taskContent.push(line);
+		});
 
 		// read file contents
-		const content = await this.app.vault.read(item.file)
+		const content = await this.app.vault.read(item.file);
 		// prepend task to new file, but put it after any frontmatter
-		const frontmatter = "---"
-		const frontmatterIndex = content.indexOf(frontmatter)
+		const frontmatter = "---";
+		const frontmatterIndex = content.indexOf(frontmatter);
 		if (frontmatterIndex !== -1) {
-			const frontmatterEnd = content.indexOf("---", frontmatterIndex + frontmatter.length)
+			const frontmatterEnd = content.indexOf(
+				"---",
+				frontmatterIndex + frontmatter.length
+			);
 			if (frontmatterEnd !== -1) {
-				const newContent = content.substring(0, frontmatterEnd + frontmatter.length) + "\n" + taskContent.join("\n") + "\n" + content.substring(frontmatterEnd + frontmatter.length)
-				await this.app.vault.modify(item.file, newContent)
+				const newContent =
+					content.substring(0, frontmatterEnd + frontmatter.length) +
+					"\n" +
+					taskContent.join("\n") +
+					"\n" +
+					content.substring(frontmatterEnd + frontmatter.length);
+				await this.app.vault.modify(item.file, newContent);
 			}
 		} else {
-			await this.app.vault.modify(item.file, taskContent.join("\n") + "\n" + content)
+			await this.app.vault.modify(
+				item.file,
+				taskContent.join("\n") + "\n" + content
+			);
 		}
 
-		console.log(taskContent)
-		new Notice("Task(s) migrated to " + item.filePath)
+		console.log(taskContent);
+		new Notice("Task(s) migrated to " + item.filePath);
 	}
 }
 
